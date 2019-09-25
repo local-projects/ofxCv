7315bf77bd3f2bda3d4f289eaacb046368976cfa
Ben Snell <bensnell@localprojects.com>
2019-09-25 10:59:55 -0400

    Added missing include

--- libs/ofxCv/include/ofxCv/Wrappers.h
+++ libs/ofxCv/include/ofxCv/Wrappers.h
@@ -28,6 +28,7 @@
 #pragma once
 
 #include "opencv2/imgproc/types_c.h"
+#include "opencv2/imgproc/imgproc_c.h"
 #include "opencv2/opencv.hpp"
 #include "ofxCv/Utilities.h"
 #include "ofVectorMath.h"
